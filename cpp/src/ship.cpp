8bf29ed32841ca573551e79fe5f710c1fbe819df
Josh R <joshrivera2203@gmail.com>
2022-12-21 17:24:46 -0900

    Added triangle collision shape for ship

--- cpp/src/ship.cpp
+++ cpp/src/ship.cpp
@@ -7,25 +7,40 @@
 using namespace std;
 
 Ship::Ship() {}
-
 void Ship::registerPhysics(b2World* world, vector2fl position)
 {
-    // object
+    // object body
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
     bodyDef.position.Set(position.x, position.y);
-    bodyDef.angle = M_PI / 2;
     body = world->CreateBody(&bodyDef);
 
+    //sets points to make triangle
+    b2Vec2 triangleVerts[3];
+    triangleVerts[0].Set((-4), (4));
+    triangleVerts[1].Set((4), (4));
+    triangleVerts[2].Set((0), (12));
+    b2PolygonShape dynamicTriangle;
+    dynamicTriangle.Set(triangleVerts, 3);
+
+    
+    b2FixtureDef fixtureHeadDef;
+    fixtureHeadDef.shape = &dynamicTriangle;
+    fixtureHeadDef.density = 1.f;
+    fixtureHeadDef.friction = 0.3;
+
+    //set points to make square base
     b2PolygonShape dynamicBox;
     dynamicBox.SetAsBox(4.f, 4.f);
     
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 1.f;
-    fixtureDef.friction = 0.3;
+    b2FixtureDef fixtureBaseDef;
+    fixtureBaseDef.shape = &dynamicBox;
+    fixtureBaseDef.density = 1.f;
+    fixtureBaseDef.friction = 0.3;
 
-    body->CreateFixture(&fixtureDef);
+    body->CreateFixture(&fixtureBaseDef);
+
+    body->CreateFixture(&fixtureHeadDef);
     body->SetAngularDamping(5);
 }
 
@@ -42,16 +57,12 @@ void Ship::handleInput()
 {
     float turnAmount = getFilteredLeftStickVector().x;
 
-    //cout << body->GetAngularVelocity() << endl;
     body->ApplyTorque(-turnAmount*SHIP_TURN_TORQUE, true);
-    
-    
-    
 
     if (sf::Joystick::isButtonPressed(0, 0))
     {
-        float angle = body->GetAngle();
-        vector2fl force = composeVector2fl(angle, SHIP_THRUST_FORCE);
+        float thrustAngle = body->GetAngle() + (M_PI/2);
+        vector2fl force = composeVector2fl(thrustAngle, SHIP_THRUST_FORCE);
         body->ApplyForceToCenter(toB2dVec(force), true);
     }
 }
